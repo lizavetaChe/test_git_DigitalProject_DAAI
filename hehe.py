06e93f0e46a0bf944798c2c657254577610c4dc2
igor9n11 <ustinov9010@gmail.com>
2025-10-05 17:19:08 +0300

    fix and add function

--- hehe.py
+++ hehe.py
@@ -1,1 +1,8 @@
+def helloworld(a):
+    if a == 'print':
+        print('Hello World!')
 helloworld('print')
+def user_name():
+    name = input("Введите имя: \n")
+    print(f"Здравствуй, {name}")
+user_name()
